ff48e6d9f1b5ac44f5fcc8a05e18f9ebea8a769e
fabio <ffonseca1985@gmail.com>
2017-11-30 05:59:34 -0200

    exemplo1

---
+++ ISMeetup/ISMeetup/AppStart/Config.cs
@@ -0,0 +1,34 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace ISMeetup.AppStart
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    AllowedScopes = { "api1" }
+                }
+            };
+        }
+    }
+}
